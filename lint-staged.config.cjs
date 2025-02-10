d91271f756348536f073f29e691208191d4808f5
Eric Vasconcelos de Oliveira <ericvascon@gmail.com>
2025-02-10 16:40:04 -0300

    fix: adjust lint-staged

---
+++ lint-staged.config.cjs
@@ -0,0 +1,15 @@
+// lint-staged.config.js
+module.exports = {
+  // Type check TypeScript files (executa uma única vez)
+  '**/*.{ts,tsx}': () => 'concurrently "tsc --noEmit" "eslint ."',
+
+  // Lint & format TS/JS files (executa nos arquivos staged)
+  '**/*.{ts,tsx,js,jsx}': (filenames) => [
+    `eslint --fix ${filenames.join(' ')}`,
+    `prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Format Markdown/JSON/CSS/etc
+  '**/*.{md,json,css,scss}': (filenames) =>
+    `prettier --write ${filenames.join(' ')}`,
+};
