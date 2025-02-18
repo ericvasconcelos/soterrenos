4fc6e1881058f0f05fd85ce0d0eeeef971e6a326
Eric Vasconcelos de Oliveira <ericvascon@gmail.com>
2025-02-18 11:36:03 -0300

    fix: adjust home infos

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,15 +2,11 @@ import { useState } from 'react';
 import reactLogo from '../../assets/react.svg';
 import viteLogo from '/vite.svg';
 import { useZipCode } from '../../hooks/useZipCode';
-import { Button, Icon, Text, Tooltip, Modal } from '@/components';
-import { iconNames } from '@/components/Icon/iconNames';
-import { IconNames } from '@/components/Icon/types';
 import './styles.css';
 
 function App() {
   const [count, setCount] = useState(0);
   const [zipCode, setZipCode] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const { data, isLoading, isError, error } = useZipCode(zipCode);
 
   return (
@@ -65,136 +61,6 @@ function App() {
           </div>
         )}
       </div>
-
-      <div className="p-8">
-        <Tooltip content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, ipsum ducimus saepe dignissimos dolore esse reprehenderit ipsa aperiam fugiat recusandae dolorum sed nobis, dolorem aut voluptas tenetur. Adipisci, deserunt saepe?">
-          <Text>Testando o tooltip</Text>
-        </Tooltip>
-      </div>
-
-      <div className="flex gap-6 mb-6">
-        <Button size="large" onClick={() => setIsModalOpen(true)}>
-          Open a super modal
-        </Button>
-      </div>
-
-      <div className="flex gap-6 mb-6">
-        <Button size="small">Small</Button>
-        <Button variant="secondary" size="small" icon="map">
-          Default
-        </Button>
-        <Button variant="tertiary" size="small">
-          Large
-        </Button>
-      </div>
-
-      <div className="flex gap-6 mb-6">
-        <Button variant="primary" color="primary">
-          Primary
-        </Button>
-        <Button variant="secondary" color="primary">
-          Secondary
-        </Button>
-        <Button variant="tertiary" color="primary">
-          Tertiary
-        </Button>
-      </div>
-
-      <div className="flex gap-6 mb-6">
-        <Button size="large">Small</Button>
-        <Button variant="secondary" size="large">
-          Default
-        </Button>
-        <Button variant="tertiary" size="large">
-          Large
-        </Button>
-      </div>
-
-      <div className="flex gap-6 mb-6">
-        <Button variant="primary" color="danger">
-          Primary
-        </Button>
-        <Button variant="secondary" color="danger">
-          Secondary
-        </Button>
-        <Button variant="tertiary" color="danger">
-          Tertiary
-        </Button>
-      </div>
-
-      <div className="flex gap-6 mb-6">
-        <Button variant="primary" color="warning">
-          Primary
-        </Button>
-        <Button variant="secondary" color="warning">
-          Secondary
-        </Button>
-        <Button variant="tertiary" color="warning">
-          Tertiary
-        </Button>
-      </div>
-
-      <div className="flex gap-6 mb-6">
-        <Button color="danger" size="small" isLoading>
-          Carregando...
-        </Button>
-        <Button color="danger" isLoading>
-          Carregando...
-        </Button>
-        <Button color="danger" size="large" isLoading>
-          Carregando...
-        </Button>
-      </div>
-
-      <div className="flex gap-6 mb-6">
-        <Button variant="secondary" color="danger" size="small" isLoading>
-          Carregando...
-        </Button>
-        <Button variant="secondary" color="danger" isLoading>
-          Carregando...
-        </Button>
-        <Button variant="secondary" color="danger" size="large" isLoading>
-          Carregando...
-        </Button>
-      </div>
-
-      <div className="flex gap-6 mb-6">
-        <Button variant="tertiary" color="danger" size="small" isLoading>
-          Carregando...
-        </Button>
-        <Button variant="tertiary" color="danger" isLoading>
-          Carregando...
-        </Button>
-        <Button variant="tertiary" color="danger" size="large" isLoading>
-          Carregando...
-        </Button>
-      </div>
-
-      <div className="flex gap-6 mb-10">
-        <Button disabled>Desabilitado</Button>
-
-        <Button variant="secondary" disabled>
-          Desabilitado
-        </Button>
-
-        <Button disabled>Desabilitado</Button>
-      </div>
-
-      <div className="flex flex-wrap gap-6">
-        {iconNames.map((name: IconNames) => (
-          <Icon key={name} name={name} color="black" />
-        ))}
-      </div>
-
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <h1 className="text-[20px] leading-6 tracking-[0.01em] md:text-center mb-6">
-          Modal Title
-        </h1>
-
-        <p className="text-[20px] font-semibold leading-6 tracking-[0.01em] md:text-center mb-8">
-          Hello, I'm a flexible modal to use whatever you want
-        </p>
-      </Modal>
     </div>
   );
 }
