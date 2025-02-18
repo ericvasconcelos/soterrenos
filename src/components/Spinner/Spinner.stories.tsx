94be1f7a492440dd3d843a1959f95875d29e0f55
Eric Vasconcelos de Oliveira <ericvascon@gmail.com>
2025-02-18 11:28:22 -0300

    docs: create Spinner story

---
+++ src/components/Spinner/Spinner.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Spinner } from '.';
+
+const meta: Meta<typeof Spinner> = {
+  title: 'Components/Spinner',
+  component: Spinner,
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: ['primary', 'danger', 'warning', 'light'],
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Spinner>;
+
+export const Default: Story = {
+  args: {
+    color: 'primary',
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    color: 'danger',
+  },
+};
