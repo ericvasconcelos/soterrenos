0a95f7045af5fc200a993d60477d65b61147fb29
Eric Vasconcelos de Oliveira <ericvascon@gmail.com>
2025-02-28 13:00:24 -0300

    fix: improve Container component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,9 +2,13 @@ import { ReactNode } from 'react';
 
 type IContainer = {
   children: ReactNode;
-  className: string;
+  className?: string;
 };
 
 export const Container = ({ children, className }: IContainer) => (
-  <div className={`container mx-auto px-8 ${className}`}>{children}</div>
+  <div
+    className={`max-w-full lg:max-w-[64rem] xl:max-w-[80rem] 2xl:max-w-[96rem] mx-auto px-8 ${className}`}
+  >
+    {children}
+  </div>
 );
