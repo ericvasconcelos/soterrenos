505fda0697eeace461e7e90ae5b38148f0789093
Eric Vasconcelos de Oliveira <ericvascon@gmail.com>
2025-02-28 13:00:38 -0300

    fix: remove margin of FormField

--- src/components/FormField/index.tsx
+++ src/components/FormField/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { IFormField } from './types';
 
 export const FormField: FC<IFormField> = ({ id, error, children }) => (
-  <div className="mb-6" {...(id && { 'data-testid': id })}>
+  <div {...(id && { 'data-testid': id })}>
     {children}
     {error && <p className="mt-2 text-xs text-danger-900">{error}</p>}
   </div>
